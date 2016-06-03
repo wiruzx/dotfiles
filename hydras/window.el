4265894f870df0512bfb3641f71393e516f85d93
Victor Shamanov <wiruzx@gmail.com>
2016-06-03 18:57:17 +0300

    Remove docstring from hydra-window

--- hydras/window.el
+++ hydras/window.el
@@ -1,13 +1,5 @@
 (defhydra hydra-window (:hint nil)
-  "
-Movement^^        ^Split^            ^Resize^                      ^Misc^
--------------------------------------------------------------------------------------
-_h_ ←           _|_ vertically       _]_ increase horizontally    _dw_ close window
-_j_ ↓           ___ horizontally     _[_ decrease horizontally    _df_ close file
-_k_ ↑                              _+_ increase vertically      _da_ close file & window
-_l_ →                              _-_ decrease vertically      _o_  close other
-                                 _=_ balance windows          _q_  exit
-"
+  "hydra window"
   ("h" windmove-left)
   ("j" windmove-down)
   ("k" windmove-up)
