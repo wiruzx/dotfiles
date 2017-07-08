ed20df986df4ef20c7448ecea8c51a94a59ed9f1
Victor Shamanov <wiruzx@gmail.com>
2017-07-08 11:47:43 +0100

    Setup hl-line

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,6 +10,12 @@
 (require 'cask "/usr/local/share/emacs/site-lisp/cask/cask.el")
 (cask-initialize)
 
+;; hl-line
+
+(global-hl-line-mode 1)
+(set-face-background 'hl-line "#3e4446")
+(set-face-foreground 'highlight nil)
+
 ;; org-bullets
 
 (require 'org-bullets)
