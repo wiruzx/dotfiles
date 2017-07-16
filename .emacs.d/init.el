a5c58a3122c611aaeac66bddc83a4d902a8b6c85
Victor Shamanov <wiruzx@gmail.com>
2017-07-16 20:09:15 +0100

    Update org-bullets list

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -23,10 +23,7 @@
 (setq org-bullets-bullet-list '(
                                 "◉"
                                 "○"
-                                "●"
-                                "✚"
-                                "◆"
-                                "▶"))
+                                "●"))
 
 ;; gist
 
