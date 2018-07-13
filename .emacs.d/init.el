2787a089549ce23ee30c0e1f07026fd514d719e6
Victor Shamanov <wiruzx@gmail.com>
2018-07-13 21:09:54 +0100

    Add switch-window

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,6 +10,21 @@
 (require 'cask "/usr/local/share/emacs/site-lisp/cask/cask.el")
 (cask-initialize)
 
+;; switch-window
+
+(require 'switch-window)
+
+(global-set-key (kbd "C-x o") 'switch-window)
+(global-set-key (kbd "C-x 1") 'switch-window-then-maximize)
+(global-set-key (kbd "C-x 2") 'switch-window-then-split-right)
+(global-set-key (kbd "C-x 3") 'switch-window-then-split-below)
+(global-set-key (kbd "C-x 0") 'switch-window-then-delete)
+
+(setq switch-window-shortcut-style 'qwerty)
+(setq switch-window-qwerty-shortcuts
+      '("a" "s" "d" "f" "j" "k" "l" ";" "w" "e" "i" "o"))
+(setq switch-window-minibuffer-shortcut ?z)
+
 ;; hl-line
 
 (global-hl-line-mode 1)
