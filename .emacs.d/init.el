86a2b504833bbea8c9aafe410a09599af48f8ff3
Victor Shamanov <wiruzx@gmail.com>
2019-05-10 14:47:32 +0100

    Remove shift-region

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -169,29 +169,6 @@
 
 (ido-mode t)
 
-;; Shift the selected region right if distance is postive, left if
-;; negative
-
-(defun shift-region (distance)
-  (let ((mark (mark)))
-    (save-excursion
-      (indent-rigidly (region-beginning) (region-end) distance)
-      (push-mark mark t t)
-      ;; Tell the command loop not to deactivate the mark
-      ;; for transient mark mode
-      (setq deactivate-mark nil))))
-
-(defun shift-right ()
-  (interactive)
-  (shift-region 1))
-
-(defun shift-left ()
-  (interactive)
-  (shift-region -1))
-
-(global-set-key [C-S-right] 'shift-right)
-(global-set-key [C-S-left] 'shift-left)
-
 ;; hydra
 
 (require 'hydra)
