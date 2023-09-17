f4eb338aa21554690a864eead951897174ef15a8
Victor Shamanov <wiruzx@gmail.com>
2023-09-18 00:53:53 +0100

    Change emacs window split behaviour

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -97,8 +97,8 @@
 (global-set-key (kbd "C-x o") 'switch-window)
 
 (global-set-key (kbd "C-x 1") 'switch-window-then-maximize)
-(global-set-key (kbd "C-x 2") 'switch-window-then-split-right)
-(global-set-key (kbd "C-x 3") 'switch-window-then-split-below)
+(global-set-key (kbd "C-x 2") 'switch-window-then-split-below)
+(global-set-key (kbd "C-x 3") 'switch-window-then-split-right)
 (global-set-key (kbd "C-x 0") 'switch-window-then-delete)
 
 (setq switch-window-shortcut-style 'qwerty)
