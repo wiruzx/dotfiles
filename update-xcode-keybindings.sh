725c1b21dd9041ab8a2f8182f71238a10994a0e3
Victor Shamanov <wiruzx@gmail.com>
2025-05-21 01:52:44 +0100

    Add reminder

--- update-xcode-keybindings.sh
+++ update-xcode-keybindings.sh
@@ -3,3 +3,5 @@ echo "CMD-D - selected test"
 echo "CMD-G - again"
 echo "CMD-Y - swap with CMD-SHIFT-Y"
 echo "CMD-O - swap with CMD-SHIFT-O"
+
+echo "Don't forget to disable all behaviours except the notifications"
